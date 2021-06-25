12c5d05d5ad99dc1f5c55bb96f134d9e7cc0badc
adeline-huvet <adeline.huvet.emploi@gmail.com>
2021-06-25 13:34:20 +0200

    Erreur maildev

--- src/Notification/ContactNotification.php
+++ src/Notification/ContactNotification.php
@@ -3,15 +3,29 @@
 namespace App\Notification;
 
 use App\Entity\Contact;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Mailer\MailerInterface;
 
 
 
 class ContactNotification
 
 {
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer; 
+    }
+
     public function notify(Contact $contact)
     {
-        
+        $email = (new TemplatedEmail())
+            ->from("contact@progica.fr")
+            ->to($contact->getEmail())
+            ->subject("Demande de contact")
+            ->htmlTemplate('Notification/contact.html.twig')
+            ->context(['contact' => $contact]);
+            $this->mailer->send($email);
     }
 
 }
